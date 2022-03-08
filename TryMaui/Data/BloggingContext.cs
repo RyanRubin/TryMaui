8416fdf568cf6c70a4d42fe423a665ffc3414926
raessatech5 <raessatech5@gmail.com>
2022-03-08 23:54:35 +0800

    ef core sqlite on maui woo-hoo

---
+++ TryMaui/Data/BloggingContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryMaui.Data
+{
+    public class BloggingContext: DbContext
+    {
+        public BloggingContext()
+        {
+            SQLitePCL.Batteries_V2.Init();
+
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "blogs.db3");
+
+            optionsBuilder
+                .UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
--- TryMaui/Pages/GitHubUsersPage.xaml.cs
+++ TryMaui/Pages/GitHubUsersPage.xaml.cs
@@ -1,9 +1,35 @@
+using TryMaui.Data;
+
 namespace TryMaui;
 
 public partial class GitHubUsersPage : ContentPage
 {
-	public GitHubUsersPage()
-	{
-		InitializeComponent();
-	}
+    public GitHubUsersPage()
+    {
+        InitializeComponent();
+
+        using (var blogContext = new BloggingContext())
+        {
+            var theBlogs = blogContext.Blogs.ToList();
+
+            if (!theBlogs.Any())
+            {
+                blogContext.Add(new Blog { Url = "a.com" });
+                blogContext.Add(new Blog { Url = "b.com" });
+                blogContext.Add(new Blog { Url = "c.com" });
+
+                blogContext.SaveChanges();
+            }
+        }
+    }
+
+    private void TestButton_Clicked(object sender, EventArgs e)
+    {
+        using (var blogContext = new BloggingContext())
+        {
+            var theBlogs = blogContext.Blogs.ToList();
+
+            var firstUrl = theBlogs.Select(o => o.Url).FirstOrDefault();
+        }
+    }
 }
